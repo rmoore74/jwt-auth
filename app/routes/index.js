f2023fc38434435f5197dce124e7ff96a4d2351d
Roger Moore <me@rogermoore.io>
2016-05-15 00:45:57 +0100

    Added index, login, register views and created routes file. Updated server.js.

---
+++ app/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('index', { title: 'JWT Auth Example' });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login');
+});
+
+router.get('/register', function(req, res) {
+    res.render('register');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
+// set up modules
 var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
 var app = express();
 
-app.get('/', function(req, res) {
-    res.json({ 'success': 'true' });
-});
+// set up view engine
+app.set('views', path.join(__dirname, 'app/views/'));
+app.set('view engine', 'pug');
 
+// set up logging, bodyparser, cookieparser
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// make css and images static
+app.use('/images', express.static(path.join(__dirname) + '/app/views/images'));
+app.use('/styles', express.static(path.join(__dirname) + '/app/views/styles'));
+
+// set up routes
+app.use('/', require('./app/routes/index'));
+//app.use('/app', require('./app/routes/users'));
+
+// start server on port 3000
 app.listen(3000, function() {
     console.log("Server listening on port 3000");
 });
